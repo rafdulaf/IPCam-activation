37fc311bc6bad9599e772ea8e0bf66c84d7d6898
Raphael Franchet <raphael.franchet@anyware-services.com>
2015-01-09 22:20:44 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # IPCam-activation
 Shell that activate/deactivate motion detection on IP cam, depending if smartphones are present on wifi
+
+Copy the script "watch-ip-cam.sh" to your home directory and add it to your crontab (using "crontab -e") and adding:
+    * * * * * /home/pi/watch-ip-cam.sh >/tmp/log 2>&1
---
+++ watch-ip-cam.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# IPCamera activate script v1.0.0
+
+# CUSTOMIZE THIS PART
+
+# The smartphones IPs (has to be fixed ips)
+IPS=( "192.168.1.X" "192.168.1.Y" )
+# The IPCamera IP (has to be fixed)
+CAMIP="192.168.1.Z"
+# The IPCamera username
+CAMUSR="USERNAME"
+# The IPCamera password
+CAMPWD="PASSWORD"
+
+# END OF CUSTOMIZABLE PAR
+
+URL_END="\" -O /dev/null"
+
+URL="wget --user=$CAMUSR --password=$CAMPWD \"http://$CAMIP"
+
+URL_SETMOTION="$URL/setSystemMotion?ConfigSystemMotion=Save&ReplyErrorPage=motion.htm&ReplySuccessPage=motion.htm&MotionDetectionScheduleDay=0"
+URL_SETMOTION_OFF="$URL_SETMOTION&MotionDetectionEnable=0$URL_END"
+URL_SETMOTION_ON="$URL_SETMOTION&MotionDetectionEnable=1&MotionDetectionScheduleMode=0&MotionDetectionSensivity=70$URL_END"
+
+URL_SETSOUND="$URL/setSystemSoundDB?ConfigSystemSoundDB=Save&SoundDetectionDB=70&SoundDetectionScheduleDay=0&ReplyErrorPage=sounddb.htm&ReplySuccessPage=sounddb.htm"
+URL_SETSOUND_OFF="$URL_SETSOUND&SoundDetectionEnable=0$URL_END"
+URL_SETSOUND_ON="$URL_SETSOUND&SoundDetectionEnable=1$URL_END"
+
+URL_MOVETO="$URL/pantiltcontrol.cgi"
+URL_MOVETO_OFF="$URL_MOVETO?PanTiltPresetPositionMove=2$URL_END"
+URL_MOVETO_ON="$URL_MOVETO?PanTiltPresetPositionMove=0$URL_END"
+
+STATEFILE=/tmp/state
+CAMREACHABLE=`ping -c 4 $CAMIP | grep "packet loss" | sed "s/^.* \([0-9]\+\)% packet loss.*$/\1/"`
+
+if [ $CAMREACHABLE != "100" ]; then
+
+        if [ -f $STATEFILE ]; then
+                STATE=`cat $STATEFILE`
+        fi
+
+        FOUND=false
+        for i in ${IPS[@]}; do
+                REACH=`ping -c 4 $i | grep "packet loss" | sed "s/^.* \([0-9]\+\)% packet loss.*$/\1/"`
+                if [ $REACH != "100" ]; then
+                        echo "FOUND "$i
+                        if [[ -z "$STATE" ]] || [ $STATE == "ON" ]; then
+                                echo "State changed to off"
+                                STATE="OFF"
+                                eval "$URL_SETMOTION_OFF"
+                                eval "$URL_MOVETO_OFF"
+                                eval "$URL_SETSOUND_OFF"
+                        fi
+                        FOUND=true
+                        break
+                fi
+        done
+
+        if [ $FOUND == false ]; then
+                echo "NOT FOUND ANY"
+
+                if [[ -z "$STATE" ]] || [ $STATE == "OFF"  ]; then
+                        echo "State changed to on"
+                        STATE="ON"
+                        eval "$URL_SETMOTION_ON"
+                        eval "$URL_MOVETO_ON"
+                        eval "$URL_SETSOUND_ON"
+                fi
+        fi
+
+        echo $STATE > $STATEFILE
+
+else
+
+        rm $STATEFILE
+
+fi
+
+echo "STATE is "$STATE
+        
